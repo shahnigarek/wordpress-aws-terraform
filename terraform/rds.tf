dc5df814837603af82318d8f243cf57586555649
shahnigarek <shahnigar@T3SFRCE.localdomain>
2025-12-12 23:31:54 +0400

    Changes made

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -22,7 +22,7 @@ resource "aws_db_instance" "mariadb" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds.id]
 
-  backup_retention_period = 7
+  backup_retention_period = 1
   backup_window           = "03:00-04:00"
   maintenance_window      = "mon:04:00-mon:05:00"
 
